f9f7c219e9aca496cf08623a15345e66825004ea
rafael <rafaelsouza@sistemafieg.org.br>
2020-03-06 15:09:57 -0300

    config working and tested with server

--- src/main/index.js
+++ src/main/index.js
@@ -5,20 +5,32 @@ const path = require('path')
 
 // const staticPath = isDev ? __dirname.replace(/src[\\||\/]main/g,'static'): __dirname.replace(/app\.asar/g,'static');
 // const pluginsFolder = path.join(staticPath,path.sep,'plugins')
-const pluginsFolder = path.join(__static,path.sep,'plugins')
-const pluginPath = path.join(pluginsFolder,path.sep,'plugin.txt');
-console.log('loading from ',pluginPath);
+const pluginsFolder = path.join(__static, path.sep, 'plugins')
+const pluginPath = path.join(pluginsFolder, path.sep, 'plugin.txt');
+console.log('loading from ', pluginPath);
 
-const pluginContent = fs.readFileSync(pluginPath,'utf8');
+const pluginContent = fs.readFileSync(pluginPath, 'utf8');
 console.log(pluginContent);
 
 let mainWindow;
 app.whenReady().then(() => {
 
     if (!isDev) {
-        const server = 'https://your-deployment-url.com'
-        const feed = `${server}/update/${process.platform}/${app.getVersion()}`
-        //autoUpdater.setFeedURL(feed)
+        const { autoUpdater } = require("electron-updater")
+        autoUpdater.logger = require("electron-log")
+        autoUpdater.logger.transports.file.level = "info"
+
+        autoUpdater.on('update-downloaded', (ev, info) => {
+            // Wait 5 seconds, then quit and install
+            // In your application, you don't need to wait 5 seconds.
+            // You could call autoUpdater.quitAndInstall(); immediately
+            setTimeout(function () {
+                autoUpdater.quitAndInstall();
+            }, 5000)
+        });
+
+        autoUpdater.checkForUpdates();
+
     } else {
         console.log('production')
     }
@@ -29,7 +41,7 @@ app.whenReady().then(() => {
     });
     mainWindow.loadURL(pluginContent);
     mainWindow.maximize();
-    mainWindow.on('close',()=>{
+    mainWindow.on('close', () => {
         mainWindow.destroy();
         app.quit();
     })
